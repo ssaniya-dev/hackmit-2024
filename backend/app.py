44d15c11439eb0bc2e0dacdec7a679e19f148800
Saniya Savla <ssaniyasavla@gmail.com>
2024-09-14 20:42:13 -0400

    added basic flask script

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify # type: ignore
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/run-script', methods=['GET'])
+def run_script():
+    try:
+        result = subprocess.run(['python3', 'test.py'], capture_output=True, text=True)
+        return jsonify({
+            'status': 'success',
+            'output': result.stdout,
+        })
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'output': str(e)
+        })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
